e42a069a15fcd77b64d5dd3caa789e893bd3ba45
Santiago Gilabert <santiagogilabert@gmail.com>
2016-04-22 16:15:06 -0300

    Install dep from tar bals under /libs folder

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -3,6 +3,17 @@
 PYTHONPATH=/opt/bsdradius/lib/python2.7/site-packages/
 export PYTHONPATH
 
-easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ requests
-easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ psycopg2
-easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ celery
+#easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ requests
+#easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ psycopg2
+#easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ celery
+
+easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ /opt/bsdradius/libs/requests-2.3.0.tar.gz
+easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ /opt/bsdradius/libs/psycopg2-2.5.1.tar.gz
+easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ /opt/bsdradius/libs/anyjson-0.3.3.tar.gz
+easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ /opt/bsdradius/libs/amqp-1.4.6.tar.gz
+easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ /opt/bsdradius/libs/kombu-3.0.26.tar.gz
+easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ /opt/bsdradius/libs/billiard-3.3.0.20.tar.gz
+easy_install -d /opt/bsdradius/lib/python2.7/site-packages/ /opt/bsdradius/libs/celery-3.1.17.tar.gz
+
+
+
