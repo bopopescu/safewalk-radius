4fbb396a16e33f8669602ff5383208a531981891
Alejandro Taboada <ataboada@gmail.com>
2017-07-14 16:58:12 -0300

    https://altipeak.plan.io/issues/1625

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
-python setup.py install --prefix=/opt/bsdradius
-#bin/install-dependencies.sh
+PREFIX=/opt/bsdradius
+python setup.py install --prefix=$PREFIX
+bin/install-dependencies.sh
+sed -i 's|clients_query =.*|clients_query = select address, name, secret, forward_reply_items from gaia_radius_interface_radiusclient|' $PREFIX/etc/bsdradius/bsdradiusd.conf | grep clients_query
+sed -i '/forward_reply_items/d' $PREFIX/etc/bsdradius/clients.conf
+echo "forward_reply_items = false" >> $PREFIX/etc/bsdradius/clients.conf
