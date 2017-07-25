38ef9bf65d8942c5ff98cf4c2b85bbbb2ce4dee9
root <root@safewalk-bdr.(none)>
2017-07-25 15:47:36 -0300

    fix version extraction

--- bin/install.sh
+++ bin/install.sh
@@ -9,7 +9,14 @@ PREFIX=/opt/bsdradius
 
 python setup.py install --prefix=$PREFIX
 RADIUS_SECRET=$(awk -F "=" '/RADIUS_SECRET/ {print $2}' $SAFEWALK_SERVER_DIR/gaia_server/common_settings.py | tr -d '"')
-RADIUS_VERSION=$(bin/bsdradiusd -v)
+echo "secret: $RADIUS_SECRET"
+
+
+VERSION_MAJOR=$(cat bsdradius/version.py | grep "major =" | grep -Eo "[0-9]" | xargs)
+VERSION_MINOR=$(cat bsdradius/version.py | grep "minor =" | grep -Eo "[0-9]" | xargs)
+VERSION_DEBUG=$(cat bsdradius/version.py | grep "debug =" | grep -Eo "[0-9]" | xargs)
+RADIUS_VERSION=$VERSION_MAJOR.$VERSION_MINOR.$VERSION_DEBUG
+echo "version: $RADIUS_VERSION"
 
 bin/install-dependencies.sh
 sed -i 's|clients_query =.*|clients_query = select address, name, secret, forward_reply_items from gaia_radius_interface_radiusclient|' $PREFIX/etc/bsdradius/bsdradiusd.conf | grep clients_query
