9358060930a97256196a9af79dfb8e9e9a1b2b6b
Alejandro Taboada <ataboada@gmail.com>
2017-10-14 11:38:45 -0300

    https://altipeak.plan.io/issues/1709 Safewalk Radius. Missing dependency urllib3

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -3,11 +3,12 @@
 PYTHONPATH=/opt/bsdradius/lib/python2.7/site-packages/
 export PYTHONPATH
 
-#easy_install -d $PYTHONPATH requests
-#easy_install -d $PYTHONPATH psycopg2
-#easy_install -d $PYTHONPATH celery
 
 easy_install -d $PYTHONPATH /opt/bsdradius/libs/pytz-2015.4.tar.gz
+easy_install -d $PYTHONPATH /opt/bsdradius/libs/chardet-3.0.4.tar.gz
+easy_install -d $PYTHONPATH /opt/bsdradius/libs/idna-2.6.tar.gz
+easy_install -d $PYTHONPATH /opt/bsdradius/libs/certifi-2017.7.27.1.tar.gz
+easy_install -d $PYTHONPATH /opt/bsdradius/libs/urllib3-1.22.tar.gz
 easy_install -d $PYTHONPATH /opt/bsdradius/libs/requests-2.18.4.tar.gz
 easy_install -d $PYTHONPATH /opt/bsdradius/libs/psycopg2-2.5.1.tar.gz
 easy_install -d $PYTHONPATH /opt/bsdradius/libs/anyjson-0.3.3.tar.gz
