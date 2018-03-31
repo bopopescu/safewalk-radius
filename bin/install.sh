fe4a7284041d3b361f546a93165bf6422aea17c8
Alejandro Taboada <ataboada@gmail.com>
2018-03-31 14:46:29 -0300

    remove version setup

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ "$1" = "" ]; then
-    SAFEWALK_SERVER_DIR=/home/safewalk/safewalk_server/sources
-else
-    SAFEWALK_SERVER_DIR=$1
-fi
+#if [ "$1" = "" ]; then
+#    SAFEWALK_SERVER_DIR=/home/safewalk/safewalk_server/sources
+#else
+#    SAFEWALK_SERVER_DIR=$1
+#fi
 PREFIX=/opt/bsdradius
 
 python setup.py install --prefix=$PREFIX
@@ -21,7 +21,7 @@ echo "version: $RADIUS_VERSION"
 bin/install-dependencies.sh
 sed -i "s|swk_auth_port.*=.*|swk_auth_port=8445|" $PREFIX/etc/bsdradius/bsdradiusd.conf
 sed -i "s|swk_protocol.*=.*|swk_protocol=https|" $PREFIX/etc/bsdradius/bsdradiusd.conf
-sed -i "s|RADIUS_VERSION.*=.*|RADIUS_VERSION = '$RADIUS_VERSION'|" $SAFEWALK_SERVER_DIR/gaia_server/common_settings.py
+#sed -i "s|RADIUS_VERSION.*=.*|RADIUS_VERSION = '$RADIUS_VERSION'|" $SAFEWALK_SERVER_DIR/gaia_server/common_settings.py
 
 #su - postgres -c "psql safewalk-server -c \"SELECT T.token FROM oauth2_client C, oauth2_accesstoken T, accounts_appuser U WHERE C.name='management-console' and T.client_id=C.id and T.user_id=U.id and U.username='radius' and U.is_system_user=True\" | head -n 3 | tail -n 1 | xargs > /tmp/rat"
 
