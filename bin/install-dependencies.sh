26208c35193e47749cff5184cac082be0ee43042
Alejandro Taboada <ataboada@gmail.com>
2018-05-24 22:44:18 +0300

    Upgrade kombu library

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -13,7 +13,8 @@ easy_install -d $PYTHONPATH /opt/bsdradius/libs/requests-2.18.4.tar.gz
 easy_install -d $PYTHONPATH /opt/bsdradius/libs/psycopg2-2.5.1.tar.gz
 easy_install -d $PYTHONPATH /opt/bsdradius/libs/anyjson-0.3.3.tar.gz
 easy_install -d $PYTHONPATH /opt/bsdradius/libs/amqp-1.4.6.tar.gz
-easy_install -d $PYTHONPATH /opt/bsdradius/libs/kombu-3.0.26.tar.gz
+#easy_install -d $PYTHONPATH /opt/bsdradius/libs/kombu-3.0.26.tar.gz
+easy_install -d $PYTHONPATH /opt/bsdradius/libs/kombu-3.0.37.tar.gz
 easy_install -d $PYTHONPATH /opt/bsdradius/libs/billiard-3.3.0.20.tar.gz
 easy_install -d $PYTHONPATH /opt/bsdradius/libs/celery-3.1.17.tar.gz
 
